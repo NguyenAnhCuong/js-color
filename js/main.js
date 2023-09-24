95196a3bbc120268e258ed9ac99e7b43cd4ad42b
NguyenAnhCuong <cuong1606x@gmail.com>
2023-09-24 14:32:21 +0700

    half way

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-import { GAME_STATUS } from './constants'
+import { GAME_STATUS, PAIRS_COUNT } from './constants.js'
+import { getColorElementList, getColorListElement } from './selectors.js';
+import { getRandomColorPairs } from './utils.js';
 
 // Global variables
 let selections = []
@@ -10,3 +12,31 @@ let gameState = GAME_STATUS.PLAYING
 // 3. Check win logic
 // 4. Add timer
 // 5. Handle replay click
+// console.log(getRandomColorPairs(PAIRS_COUNT));
+function handleColorClick(liElement){
+    console.log(liElement);
+    liElement.classList.add('active')
+}
+function initColor(){
+    //random 8 pair color
+    //bind to li > div.overlay
+    const colorList = getRandomColorPairs(PAIRS_COUNT);
+    const liList = getColorElementList();
+    liList.forEach((liElement,index) => {
+        const overlayElement = liElement.querySelector('.overlay')
+        if (overlayElement) overlayElement.style.backgroundColor = colorList[index];
+    })
+}
+function AttachEventForColorList(){
+    const ulElement = getColorListElement();
+    if (!ulElement) return;
+    ulElement.addEventListener('click',(event) => {
+        console.log(event.target);
+        handleColorClick(event.target);
+    })
+
+}
+(() => {
+    initColor();
+    AttachEventForColorList()
+})()
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,29 @@
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+  const colorList = []
+  const hueList = ['red','orange','yellow','green','blue','purple','pink','monochrome']
+
+  //random count color
+  for (let i = 0;i< count;i++){
+    const color = window.randomColor({
+      liminosity : 'dark',
+      hue : hueList[i % hueList.length],
+    })
+    colorList.push(color);
+  }
+
+  const fullColorList = [...colorList,...colorList];
+  shuffle(fullColorList);
+
+  return fullColorList;
+}
+function shuffle(arr){
+  if (!Array.isArray(arr) || arr.length <= 2) return arr;
+  for (let i = arr.length -1;i > 1;i--){
+    const j = Math.floor(Math.random() * i);
+    let temp = arr[i];
+    arr[i] = arr[j]
+    arr[i] = temp;
+  }
 }
